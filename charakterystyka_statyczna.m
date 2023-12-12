7a5d0d06139b54a31f61161a7c1021efdacb2f9f
Krystian Piszczela <01168907@pw.edu.pl>
2023-12-12 19:45:48 +0100

    zad1

---
+++ charakterystyka_statyczna.m
@@ -0,0 +1,18 @@
+clear
+dane();
+u=u_min:0.05:u_max;
+stala = (beta1+beta2)/(1 + alfa1 + alfa2);
+
+for i=1:size(u,2)
+    g1 = (exp(4.75*u(i))-1)/(exp(4.75*u(i))+1);
+    y(i) = 1-exp(stala*g1);
+end
+
+figure
+plot(u,y);
+xlabel('Sygnał wejściowy u');
+ylabel('Sygnał wyjściowy y');
+title('Charakterystyka statyczna y(u)');
+plot_data = [u' y'];
+save('wyniki/zad1_char_stat.txt','plot_data', '-ascii')
+
---
+++ symulacja_procesu.m
@@ -0,0 +1,51 @@
+clear
+dane()
+kp=4;
+kk=2000;
+
+u_ucz = zeros(kk,1);
+x1_ucz = zeros(kk,1);
+x2_ucz = zeros(kk,1);
+g1_ucz = zeros(kk,1);
+y_ucz = zeros(kk,1);
+
+u_wer = zeros(kk,1);
+x1_wer = zeros(kk,1);
+x2_wer = zeros(kk,1);
+g1_wer = zeros(kk,1);
+y_wer = zeros(kk,1);
+
+for i=1:50:kk
+    u_ucz(i:i+50) = -1 + 2*rand;
+    u_wer(i:i+50) = -1 + 2*rand;
+end
+
+for k=kp:kk
+    g1_ucz(k-3) = (exp(4.75*u_ucz(k-3))-1)/(exp(4.75*u_ucz(k-3))+1);
+    x1_ucz(k) = -alfa1*x1_ucz(k-1) + x2_ucz(k-1) + beta1*g1_ucz(k-3);
+    x2_ucz(k) = -alfa2*x1_ucz(k-1) + beta2*g1_ucz(k-3);
+    y_ucz(k) = 1-exp(x1_ucz(k));
+
+    g1_wer(k-3) = (exp(4.75*u_wer(k-3))-1)/(exp(4.75*u_wer(k-3))+1);
+    x1_wer(k) = -alfa1*x1_wer(k-1) + x2_wer(k-1) + beta1*g1_wer(k-3);
+    x2_wer(k) = -alfa2*x1_wer(k-1) + beta2*g1_wer(k-3);
+    y_wer(k) = 1-exp(x1_wer(k));
+end
+
+
+t = 1:kk;
+figure
+plot(y_ucz)
+xlabel('Numer próbki k');
+ylabel('Sygnał wyjściowy y');
+title('Dane uczące');
+plot_data = [t' y_ucz];
+save('wyniki/zad1_dane_uczace.txt','plot_data', '-ascii')
+
+figure
+plot(y_wer)
+xlabel('Numer próbki k');
+ylabel('Sygnał wyjściowy y');
+title('Dane weryfikujące');
+plot_data = [t' y_wer];
+save('wyniki/zad1_dane_weryfikujace.txt','plot_data', '-ascii')
